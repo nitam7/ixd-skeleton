d08f36446a9c75df691e32710f7b828bc8a0dba0
nitam7 <nitam@ucsd.edu>
2018-02-21 16:16:05 -0800

    fixed up Facebook

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -6,12 +6,18 @@ function checkLoginState() {
 
 function changeUser(response) {
 	$(".facebookLogin").hide();
-	$("p:first").text(response.name);
+	$("h4:first").text(response.name);
+	$(".signingIn").hide();
+	$("signingUp").hide();
+	alert("You are logged in as: " + response.name);
+	location.href = "./index";
 }
 
 function statusChangeCallback(response) {
+	console.log('Facebook login status changed');
+	console.log(response);
 	if(response.status === 'connected') {
 		console.log('Successfully logged in with Facebook');
-		FB.api('/me?fields=name,first_name, changeUser')
+		FB.api('/me?fields=name,first_name', changeUser);
 	}
 }
