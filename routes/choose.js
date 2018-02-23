7e8b90c806e1d69caf395b770587cf4010400269
jaime perdue <jperdue@ucsd.edu>
2018-02-22 22:32:29 -0800

    trying to save data

--- routes/choose.js
+++ routes/choose.js
@@ -2,6 +2,35 @@
 
 var data = require("../data.json");
 
+
+
+
+
+var fs = require('fs')
+
+fs.readFile('./users.json', 'utf-8', function(err, data) {
+	if (err) throw err
+
+	var arrayOfObjects = JSON.parse(data)
+	arrayOfObjects.users.push({
+		url: "chosenAnimal"
+	})
+
+	console.log(arrayOfObjects)
+
+	fs.writeFile('./users.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+		if (err) throw err
+		console.log('Done!')
+	})
+})
+
+
+
+
+
+			   
+
+
 exports.view = function(req, res){
 	console.log(data);
   	res.render('choose', data);
