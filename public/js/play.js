f9145dedb843e3d67c960df335ee9901791c1f9c
nitam7 <nitam@ucsd.edu>
2018-02-28 13:30:12 -0800

    finished up graph -- based on most recent 5 sessions. thinking of adding annotations with date details later

--- public/js/play.js
+++ public/js/play.js
@@ -20,19 +20,28 @@ function completeTask (){
 	var percentage = productivityWidth;
 	//localStorage.setItem("percent", percentage);
 
-					var point = localStorage.getItem("percent");
-       				var d = new Date();
-        			var days = ["Sun", "M", "T", "W", "Th", "F", "Sat"];
-        			var dayToday = days[d.getDay()];
-            		if(dayToday == "Sun"){
+       				// var d = new Date();
+        			// var days = ["Sun", "M", "T", "W", "Th", "F", "Sat"];
+        			// var dayToday = days[d.getDay()];
+           //  		if(dayToday == "Sun"){
                 
-            		}
-        
+           //  		}
+           var b = JSON.parse(localStorage.getItem("percent"));
+            if (b.length >= 5) {
+                var c = []
+                localStorage.removeItem("percent");
+                localStorage.setItem("percent", JSON.stringify (c));
+                c.push(JSON.parse(localStorage.getItem("percent")));
+                console.log(localStorage.getItem("percent"));
+            }
+
         var a = [percentage];
         a = JSON.parse(localStorage.getItem("percent"));
         a.push(percentage);
         localStorage.setItem("percent", JSON.stringify(a));
         console.log(localStorage.getItem("percent"));
+
+
      // writeJSON();
  }
 
